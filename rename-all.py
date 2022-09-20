a150e2ef262229d094c7b8b795c13a4bbcd9c3cd
EmmaTheFemma <92608727+EmmaTheFemma@users.noreply.github.com>
2022-09-20 17:50:28 +0200

    Hej

--- rename-all.py
+++ rename-all.py
@@ -5,23 +5,31 @@ import shutil
 from os.path import isfile, join
 from pathlib import Path
 import reuse
-from reuse import formatName
+#from reuse import formatName
 
 
 #downloads_path = str(Path.home() / "Downloads")
 folder_path = str(Path.home() / "Downloads" / "Torrents")
 os.chdir(folder_path)
 
-pattern_anyfile = "\.[^\\]+$"
+year_pattern = "((18|19|20|21)[0-9]{2})"
+hd_pattern = "(480|720|1080)"
 
 #print(formatName("hej.dsfj."))
 
-# next(os.walk('.'))[1]
-#os.listdir(folder_path)
+def format_folder(x):
+    findyear = re.findall(year_pattern,x)
+    if findyear:
+        year = findyear[0][0]
+        print(year)
+    #hej, rest = re.split(year_pattern, x, maxsplit=1)
+
+
 def rename_all():
     for i in os.listdir(folder_path):
         if os.path.isdir(i):
             print("dir: ", i)
+            format_folder(i)
 
 
 rename_all()
